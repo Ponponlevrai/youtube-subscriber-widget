9adbe3f7ffea7e0a9ecf43f957acafd40055e7d3
Ponponlevrai <cyrusbouly@gmail.com>
2026-08-04 22:22:02 +0200

    Update script-dev.js

--- script-dev.js
+++ script-dev.js
@@ -1,1 +1,25 @@
+const workerURL = "https://ponpon-youtube-api.cyrusbouly.workers.dev/";
 
+async function updateSubscribers() {
+  try {
+    const response = await fetch(workerURL);
+    const data = await response.json();
+
+    document.getElementById("subscriberCount").textContent =
+      data.subscribers + " abonnés";
+
+    document.getElementById("channelName").textContent =
+      data.channelName;
+
+    document.getElementById("avatar").src =
+      data.avatar;
+
+  } catch (error) {
+    document.getElementById("subscriberCount").textContent =
+      "Erreur de chargement";
+  }
+}
+
+updateSubscribers();
+
+setInterval(updateSubscribers, 60000);
