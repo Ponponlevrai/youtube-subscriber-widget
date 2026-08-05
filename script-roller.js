5bef8f5d7e78a7451360bdd549110373393a6c29
Ponponlevrai <cyrusbouly@gmail.com>
2026-08-05 11:14:07 +0200

    Update script-roller.js

--- script-roller.js
+++ script-roller.js
@@ -1,1 +1,21 @@
+const counter = document.getElementById("counter");
 
+let number = 993;
+
+function displayNumber(value){
+
+    counter.innerHTML = "";
+
+    value.toString().split("").forEach(digit=>{
+
+        const div=document.createElement("div");
+
+        div.textContent=digit;
+
+        counter.appendChild(div);
+
+    });
+
+}
+
+displayNumber(number);
