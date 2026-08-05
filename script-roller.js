93e8d401ea21fb4103cbc9fbec399b685f7d6ee6
Ponponlevrai <cyrusbouly@gmail.com>
2026-08-05 22:38:38 +0200

    Update script-roller.js

--- script-roller.js
+++ script-roller.js
@@ -20,6 +20,37 @@ let currentDigit = 3;
 
 function moveTo(digit) {
 
+  // Cas spécial : 9 -> 0
+  if (currentDigit === 9 && digit === 0) {
+
+    // On descend jusqu'au 9 du bas
+    strip.style.transform =
+      `translateY(${-10 * digitHeight}px)`;
+
+    // Une fois l'animation terminée...
+    setTimeout(() => {
+
+      // On enlève la transition
+      strip.style.transition = "none";
+
+      // On replace instantanément sur le vrai 0
+      strip.style.transform =
+        `translateY(${-9 * digitHeight}px)`;
+
+      // On force le navigateur à appliquer le changement
+      strip.offsetHeight;
+
+      // On remet la transition
+      strip.style.transition =
+        "transform .8s ease";
+
+    }, 800);
+
+    currentDigit = 0;
+    return;
+  }
+
+  // Cas normal
   strip.style.transform =
     `translateY(${-positions[digit] * digitHeight}px)`;
 
