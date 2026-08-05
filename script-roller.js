143125abd8dc44d6085e30369eb3108a0e3173a2
Ponponlevrai <cyrusbouly@gmail.com>
2026-08-05 11:52:53 +0200

    Update script-roller.js

--- script-roller.js
+++ script-roller.js
@@ -1,7 +1,24 @@
 const container = document.querySelector(".digit-container");
+const oldDigit = document.querySelector(".old");
+const newDigit = document.querySelector(".new");
+
+
+function changeDigit(from, to) {
+
+  oldDigit.textContent = from;
+  newDigit.textContent = to;
+
+  container.classList.remove("move");
+
+  // petite pause pour remettre l'animation à zéro
+  void container.offsetWidth;
+
+  container.classList.add("move");
+}
+
 
 setTimeout(() => {
 
-  container.classList.add("move");
+  changeDigit(3, 4);
 
 }, 2000);
